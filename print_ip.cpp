865101b3679869d479986df273c88c772db5492b
Ontuzheva <puzanova679@gmail.com>
2023-02-04 22:42:14 +0300

    add list and vector

--- print_ip.cpp
+++ print_ip.cpp
@@ -3,7 +3,63 @@
 #include <list>
 #include <vector>
 
-template <typename T, typename std::enable_if<std::is_integral<T>::value, T>::type = 0>
+namespace checker
+{
+    template <typename T>
+    struct is_list_or_vector
+    {
+        enum
+        {
+            value = false
+        };
+    };
+
+    template <typename T, typename A>
+    struct is_list_or_vector<std::vector<T, A>>
+    {
+        enum
+        {
+            value = true
+        };
+    };
+
+    template <typename T, typename A>
+    struct is_list_or_vector<std::list<T, A>>
+    {
+        enum
+        {
+            value = true
+        };
+    };
+
+    template <typename T>
+    struct is_integer
+        : std::integral_constant<
+              bool,
+              std::is_integral<T>::value &&
+                  !std::is_same<bool, T>::value &&
+                  !std::is_same<char, T>::value>
+    {
+    };
+
+} // namespace checker
+
+template <typename T, typename std::enable_if<checker::is_list_or_vector<T>::value, T>::type * = nullptr>
+void print_ip(T t)
+{
+    auto last_iter = --t.end();
+    for (auto it = t.begin(); it != t.end(); ++it)
+    {
+        std::cout << *it;
+        if (it != last_iter)
+        {
+            std::cout << ".";
+        }
+    }
+    std::cout << '\n';
+}
+
+template <typename T, typename std::enable_if<checker::is_integer<T>::value, T>::type = 0>
 void print_ip(T t)
 {
     unsigned char bytes[sizeof t];
@@ -21,24 +77,21 @@ void print_ip(T t)
     std::cout << std::endl;
 }
 
-
-template <typename T,  typename std::enable_if<std::is_same<std::string, T>::value>::type* = nullptr>
+template <typename T, typename std::enable_if<std::is_same<std::string, T>::value>::type * = nullptr>
 void print_ip(T t)
 {
-    std::cout << std::endl;
+    std::cout << t << std::endl;
 }
-
 
 int main()
 {
-    print_ip(int8_t{-1});                   // 255
-    print_ip(int16_t{0});                   // 0.0
-    print_ip(int32_t{2130706433});          // 127.0.0.1
-    print_ip(int64_t{8875824491850138409}); // 123.45.67.89.101.112.131.41
-    // print_ip(std::string{"Hello, World!"});         // Hello, World!
-    // print_ip(std::vector<int>{100, 200, 300, 400}); // 100.200.300.400
-    // print_ip(std::list<short>{400, 300, 200, 100}); // 400.300.200.100
+    print_ip(int8_t{-1});                           // 255
+    print_ip(int16_t{0});                           // 0.0
+    print_ip(int32_t{2130706433});                  // 127.0.0.1
+    print_ip(int64_t{8875824491850138409});         // 123.45.67.89.101.112.131.41
+    print_ip(std::string{"Hello, World!"});         // Hello, World!
+    print_ip(std::vector<int>{100, 200, 300, 400}); // 100.200.300.400
+    print_ip(std::list<short>{400, 300, 200, 100}); // 400.300.200.100
     // print_ip(std::make_tuple(123, 456, 789, 0));    // 123.456.789.0
-
     return 0;
 }
